3d8b50b5eea7db898f491ab1c6d62336009a1760
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-01-13 12:36:13 +0500

    feat: add hamburger menu

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,3 +1,27 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  nav.classList.toggle('open');
+  setTimeout(() => {
+    navList.classList.toggle('open')
+  }, 1)
+}
+
+navList.addEventListener('click', closeMenu)
+
+function closeMenu(event) {
+  if (event.target.classList.contains('nav-link')) {
+    nav.classList.remove('open')
+    navList.classList.remove('open')
+    hamburger.classList.remove('open')
+  }
+}
+
 console.log(`
 1. [+10] Верстка валидная
 2. [+20] Верстка семантическая
