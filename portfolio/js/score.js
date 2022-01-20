9f905780974cdf476f81970e5c62302c7060072a
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-01-20 20:22:28 +0500

    refactor: split index.js into separate files

--- portfolio/js/score.js
+++ portfolio/js/score.js
@@ -1,27 +1,3 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
-const navList = document.querySelector('.nav-list');
-
-hamburger.addEventListener('click', toggleMenu)
-
-function toggleMenu() {
-  hamburger.classList.toggle('open');
-  nav.classList.toggle('open');
-  setTimeout(() => {
-    navList.classList.toggle('open')
-  }, 1)
-}
-
-navList.addEventListener('click', closeMenu)
-
-function closeMenu(event) {
-  if (event.target.classList.contains('nav-link')) {
-    nav.classList.remove('open')
-    navList.classList.remove('open')
-    hamburger.classList.remove('open')
-  }
-}
-
 console.log(`PART 1
 =======================================
 1. [+10] Верстка валидная
