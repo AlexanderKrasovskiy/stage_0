35e77de481c5a777bbd5f46d1b3cc9573dfb2c57
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-02-03 21:06:24 +0500

    feat: finish header styling

---
+++ movie-app/js/script.js
@@ -0,0 +1,18 @@
+const API_KEY = 'c3f2f908e782ed17e19472a6d81712c7';
+
+
+// Config
+// https://api.themoviedb.org/3/configuration?api_key=c3f2f908e782ed17e19472a6d81712c7
+
+// Popular
+// https://api.themoviedb.org/3/movie/popular?api_key=c3f2f908e782ed17e19472a6d81712c7&page=1
+
+// IMGs
+// https://image.tmdb.org/t/p/w342/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+// poster w342 / w500
+// other w300
+
+// Search
+// https://api.themoviedb.org/3/search/movie?api_key=c3f2f908e782ed17e19472a6d81712c7&query=Jack+Reacher
+// https://api.themoviedb.org/3/search/movie?api_key=c3f2f908e782ed17e19472a6d81712c7&query=Ava%20Tar
+// https://api.themoviedb.org/3/search/movie?api_key=c3f2f908e782ed17e19472a6d81712c7&query=batman%20robin&page=1&include_adult=true
